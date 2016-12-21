4cdaa395fc8fbb07243ab0da99ddbe865268f646
zhengaihua <zhengaihua@jd.com>
2016-12-21 09:18:06 +0800

    create project

---
+++ src/main/java/com/jd/trade/UITest/App.java
@@ -0,0 +1,13 @@
+package com.jd.trade.UITest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
